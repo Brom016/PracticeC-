2d7f4e8b4747f0238e2497fa7eefc843c7840f73
Brom016 <hamidbromo@gmail.com>
2025-01-01 08:25:16 +0700

    Update: 2025-01-01 08:25:16

--- auto_commit.sh
+++ auto_commit.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
-# Direktori proyek
 PROJECT_DIR="/c/Users/USER/Documents/Algoritma_Pemrograman/tugasbab"
 
-# Pesan commit
-COMMIT_MESSAGE="Auto commit: $(date '+%Y-%m-%d %H:%M:%S')"
+COMMIT_MESSAGE="Update: $(date '+%Y-%m-%d %H:%M:%S')"
 
-# Navigasi ke direktori proyek
 cd "$PROJECT_DIR" || exit
 
-# Tambah semua file ke staging area
 git add .
 
-# Buat commit
 git commit -m "$COMMIT_MESSAGE"
 
-# Push ke branch default (main/master)
 git push origin main
